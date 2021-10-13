d71784aa8b659fbed7a089f3291b388770f3136c
ckost9n <ckost9n@gmail.com>
2021-10-13 09:06:14 +0300

    update

---
+++ WeatherApp/VIew/Cell/AllCitiesCell.swift
@@ -0,0 +1,23 @@
+//
+//  AllCitiesCell.swift
+//  WeatherApp
+//
+//  Created by Konstantin on 12.10.2021.
+//
+
+import UIKit
+
+class AllCitiesCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
